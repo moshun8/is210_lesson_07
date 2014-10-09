7faebd064c6233d653624b7139e5bf489094c555
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-10-09 14:18:20 -0400

    fixed indentation in nesting task 1

--- task_01.py
+++ task_01.py
@@ -3,11 +3,10 @@
 """Creating a Dictionary"""
 
 
-GRADE_DATA = {'student': {
-    'Luke Skywalker': {
-    'math': 'B', 'etiquette': 'B+', 'grammar': 'B', 'gym': 'A'},
-    'Han Solo': {
-    'math': 'A-', 'etiquette': 'C-', 'grammar': 'B', 'gym': 'B'},
-    'C-3PO': {
-    'math': 'C', 'etiquette': 'A+', 'grammar': 'A', 'gym': 'F'}
-    }}
+GRADE_DATA = {'student': 
+    {'Luke Skywalker': 
+        {'math': 'B', 'etiquette': 'B+', 'grammar': 'B', 'gym': 'A'},
+    'Han Solo': 
+        {'math': 'A-', 'etiquette': 'C-', 'grammar': 'B', 'gym': 'B'},
+    'C-3PO': 
+        {'math': 'C', 'etiquette': 'A+', 'grammar': 'A', 'gym': 'F'}}}
