a9a2922e2876b50b823fb9e7a79312708e231396
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-10-12 09:37:21 -0400

    fixing task 7, pylint 8

--- task_07.py
+++ task_07.py
@@ -14,12 +14,29 @@ DATA = {
     0: 326226,
     -15: 417203,
     -56: 333525,
+    67: 323451,
+    99: 321696,
+    21: 336753,
+    -100: 361237,
+    55: 1209714,
+    5150: 1771800,
+    42: 4714011,
+    888: 14817667,
+    3500: 13760234,
+    712: 10903322,
+    7: 10443792,
+    842: 11716264,
+    18584: 10559923,
+    666: 9275602,
+    70: 11901200,
+    153: 12074784,
+    8: 4337229
 }
 
 
-def iter_dict_funky_sum(something):
+def iter_dict_funky_sum(DATA):
     """add it up"""
     running_total = 0
-    for first, second in something.iteritems():
+    for first, second in DATA.iteritems():
         running_total += second - first
-	return running_total
+    return running_total
--- task_08.py
+++ task_08.py
@@ -4,20 +4,13 @@
 
 
 from data import FRUIT
-shoplist = {'Lime': 12, 'Red Pear': 4, 'Peach': 24, 'Beet': 1}
+
 
 def get_cost_per_item(shoplist):
     """how much items cost"""
     return {item: nums * FRUIT.get(item) for item, nums in shoplist.iteritems() if item in FRUIT}
-#  .get(key) returns the value, not the key. 
-#  .values() was returning all the values, not the ones I wanted
 
 
 def get_total_cost(shoplist):
     """finds total cost"""
     return sum(get_cost_per_item(shoplist).itervalues())
-#  itervalues goes through the values like iteritems goes through items and values
-#  good website: http://python-future.org/compatible_idioms.html
-
-print get_cost_per_item({'Lime': 12, 'Red Pear': 4, 'Peach': 24, 'Beet': 1})
-print get_total_cost({'Lime': 12, 'Red Pear': 4, 'Peach': 24, 'Beet': 1})
